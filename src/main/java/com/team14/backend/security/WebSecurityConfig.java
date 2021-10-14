1b0c40079a60ddedf82d31e20a887ea45286df5f
namusik <wsnam0418@gmail.com>
2021-10-14 13:32:33 +0900

    websecurity cors 추가

--- src/main/java/com/team14/backend/security/WebSecurityConfig.java
+++ src/main/java/com/team14/backend/security/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf()
                 .disable();
 
